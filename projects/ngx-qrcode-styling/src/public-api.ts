2eb1eede8044b22fdc61feb6312c573b5d6ae90c
daidh1 <daidh@goline.vn>
2022-06-07 14:12:38 +0700

    1.1.0-rc.6

--- projects/ngx-qrcode-styling/src/public-api.ts
+++ projects/ngx-qrcode-styling/src/public-api.ts
@@ -4,5 +4,6 @@
 
 export * from './lib/ngx-qrcode-styling.templates';
 export * from './lib/ngx-qrcode-styling.options';
+export * from './lib/ngx-qrcode-styling.service';
 export * from './lib/ngx-qrcode-styling.component';
 export * from './lib/ngx-qrcode-styling.module';
